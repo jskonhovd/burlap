fc898ffa3c7ce7780b888f5343ffc90256252ccf
Jeffrey Skonhovd <jskonhovd@gmail.com>
2014-04-16 16:46:40 -0500

    Update

--- src/duck/Project3_Gridworld.java
+++ src/duck/Project3_Gridworld.java
@@ -1,22 +1,39 @@
 package duck;
 
-import burlap.behavior.singleagent.planning.StateConditionTest;
-import burlap.behavior.singleagent.planning.deterministic.TFGoalCondition;
+import java.awt.Color;
+import java.util.List;
+
+import burlap.behavior.singleagent.*;
+import burlap.domain.singleagent.gridworld.*;
+import burlap.oomdp.core.*;
+import burlap.oomdp.singleagent.*;
+import burlap.oomdp.singleagent.common.*;
 import burlap.behavior.statehashing.DiscreteStateHashFactory;
-import burlap.domain.singleagent.gridworld.GridWorldDomain;
-import burlap.domain.singleagent.gridworld.GridWorldStateParser;
-import burlap.domain.singleagent.gridworld.GridWorldVisualizer;
+import burlap.behavior.singleagent.learning.*;
+import burlap.behavior.singleagent.learning.tdmethods.*;
+import burlap.behavior.singleagent.planning.*;
+import burlap.behavior.singleagent.planning.commonpolicies.GreedyQPolicy;
+import burlap.behavior.singleagent.planning.deterministic.*;
+import burlap.behavior.singleagent.planning.deterministic.informed.Heuristic;
+import burlap.behavior.singleagent.planning.deterministic.informed.astar.AStar;
+import burlap.behavior.singleagent.planning.deterministic.uninformed.bfs.BFS;
+import burlap.behavior.singleagent.planning.deterministic.uninformed.dfs.DFS;
+import burlap.behavior.singleagent.planning.stochastic.valueiteration.ValueIteration;
+import burlap.behavior.singleagent.planning.stochastic.policyiteration.*;
+import burlap.oomdp.visualizer.Visualizer;
+import burlap.oomdp.auxiliary.StateGenerator;
 import burlap.oomdp.auxiliary.StateParser;
-import burlap.oomdp.core.Domain;
-import burlap.oomdp.core.State;
-import burlap.oomdp.core.TerminalFunction;
-import burlap.oomdp.singleagent.RewardFunction;
-import burlap.oomdp.singleagent.SADomain;
-import burlap.oomdp.singleagent.common.SinglePFTF;
-import burlap.oomdp.singleagent.common.UniformCostRF;
+import burlap.oomdp.auxiliary.common.ConstantStateGenerator;
+import burlap.behavior.singleagent.EpisodeSequenceVisualizer;
+import burlap.behavior.singleagent.auxiliary.StateReachability;
+import burlap.behavior.singleagent.auxiliary.performance.LearningAlgorithmExperimenter;
+import burlap.behavior.singleagent.auxiliary.performance.PerformanceMetric;
+import burlap.behavior.singleagent.auxiliary.performance.TrialMode;
+import burlap.behavior.singleagent.auxiliary.valuefunctionvis.ValueFunctionVisualizerGUI;
+import burlap.behavior.singleagent.auxiliary.valuefunctionvis.common.*;
+import burlap.behavior.singleagent.auxiliary.valuefunctionvis.common.PolicyGlyphPainter2D.PolicyGlyphRenderStyle;
 import burlap.oomdp.singleagent.common.VisualActionObserver;
 import burlap.oomdp.singleagent.explorer.VisualExplorer;
-import burlap.oomdp.visualizer.Visualizer;
 
 public class Project3_Gridworld {
 	GridWorldDomain				gwdg;
@@ -56,6 +73,9 @@ public class Project3_Gridworld {
 		GridWorldDomain.setAgent(initialState, 0, 0);
 		GridWorldDomain.setLocation(initialState, 0, 10, 10);
 		
+	
+
+			
 		//set up the state hashing system
 		hashingFactory = new DiscreteStateHashFactory();
 		hashingFactory.setAttributesForClass(GridWorldDomain.CLASSAGENT, 
@@ -64,8 +84,132 @@ public class Project3_Gridworld {
 		VisualActionObserver observer = new VisualActionObserver(domain, 
 				GridWorldVisualizer.getVisualizer(domain, gwdg.getMap()));
 	((SADomain) this.domain).setActionObserverForAllAction(observer);
-	//observer.initGUI();
+	observer.initGUI();
 		
+	}
+	
+	public void valueFunctionVisualize(QComputablePlanner planner, Policy p){
+		List <State> allStates = StateReachability.getReachableStates(initialState, 
+			(SADomain)domain, hashingFactory);
+		LandmarkColorBlendInterpolation rb = new LandmarkColorBlendInterpolation();
+		rb.addNextLandMark(0., Color.RED);
+		rb.addNextLandMark(1., Color.BLUE);
+		
+		StateValuePainter2D svp = new StateValuePainter2D(rb);
+		svp.setXYAttByObjectClass(GridWorldDomain.CLASSAGENT, GridWorldDomain.ATTX, 
+			GridWorldDomain.CLASSAGENT, GridWorldDomain.ATTY);
+		
+		PolicyGlyphPainter2D spp = new PolicyGlyphPainter2D();
+		spp.setXYAttByObjectClass(GridWorldDomain.CLASSAGENT, GridWorldDomain.ATTX, 
+			GridWorldDomain.CLASSAGENT, GridWorldDomain.ATTY);
+		spp.setActionNameGlyphPainter(GridWorldDomain.ACTIONNORTH, new ArrowActionGlyph(0));
+		spp.setActionNameGlyphPainter(GridWorldDomain.ACTIONSOUTH, new ArrowActionGlyph(1));
+		spp.setActionNameGlyphPainter(GridWorldDomain.ACTIONEAST, new ArrowActionGlyph(2));
+		spp.setActionNameGlyphPainter(GridWorldDomain.ACTIONWEST, new ArrowActionGlyph(3));
+		spp.setRenderStyle(PolicyGlyphRenderStyle.DISTSCALED);
+		
+		ValueFunctionVisualizerGUI gui = new ValueFunctionVisualizerGUI(allStates, svp, planner);
+		gui.setSpp(spp);
+		gui.setPolicy(p);
+		gui.setBgColor(Color.GRAY);
+		gui.initGUI();
+	}
+	
+	public void ValueIterationExample(String outputPath){
+		
+		if(!outputPath.endsWith("/")){
+			outputPath = outputPath + "/";
+		}
+		
+		
+		OOMDPPlanner planner = new ValueIteration(domain, rf, tf, 0.99, hashingFactory,
+								0.001, 100);
+		planner.planFromState(initialState);
+		
+		//create a Q-greedy policy from the planner
+		Policy p = new GreedyQPolicy((QComputablePlanner)planner);
+		
+		//record the plan results to a file
+		p.evaluateBehavior(initialState, rf, tf).writeToFile(outputPath + "planResult", sp);
+		
+		//visualize the value function and policy
+		this.valueFunctionVisualize((QComputablePlanner)planner, p);
+		
+	}
+	
+	public void PolicyIterationExample(String outputPath)
+	{
+		if(!outputPath.endsWith("/")){
+			outputPath = outputPath + "/";
+		}
+		
+		
+		OOMDPPlanner planner = new PolicyIteration(domain, rf, tf, 0.99, hashingFactory,
+								0.001, 100, 100);
+		planner.planFromState(initialState);
+		
+		//create a Q-greedy policy from the planner
+		Policy p = new GreedyQPolicy((QComputablePlanner)planner);
+		
+		//record the plan results to a file
+		p.evaluateBehavior(initialState, rf, tf).writeToFile(outputPath + "planResult", sp);
+		
+		//visualize the value function and policy
+		this.valueFunctionVisualize((QComputablePlanner)planner, p);
+		
+	}
+	
+public void experimenterAndPlotter(){
+		
+		//custom reward function for more interesting results
+		final RewardFunction rf = new GoalBasedRF(this.goalCondition, 5., -0.1);
+
+		/**
+		 * Create factories for Q-learning agent and SARSA agent to compare
+		 */
+
+		LearningAgentFactory qLearningFactory = new LearningAgentFactory() {
+			
+			@Override
+			public String getAgentName() {
+				return "Q-learning";
+			}
+			
+			@Override
+			public LearningAgent generateAgent() {
+				return new QLearning(domain, rf, tf, 0.99, hashingFactory, 0.3, 0.1);
+			}
+		};
+
+
+		LearningAgentFactory sarsaLearningFactory = new LearningAgentFactory() {
+			
+			@Override
+			public String getAgentName() {
+				return "SARSA";
+			}
+			
+			@Override
+			public LearningAgent generateAgent() {
+				return new SarsaLam(domain, rf, tf, 0.99, hashingFactory, 0.0, 0.1, 1.);
+			}
+		};
+
+		StateGenerator sg = new ConstantStateGenerator(this.initialState);
+
+		LearningAlgorithmExperimenter exp = new LearningAlgorithmExperimenter((SADomain)this.domain, 
+			rf, sg, 10, 100, qLearningFactory, sarsaLearningFactory);
+
+		exp.setUpPlottingConfiguration(500, 250, 2, 1000, 
+			TrialMode.MOSTRECENTANDAVERAGE, 
+			PerformanceMetric.CUMULATIVESTEPSPEREPISODE, 
+			PerformanceMetric.AVERAGEEPISODEREWARD);
+
+		exp.startExperiment();
+
+		exp.writeStepAndEpisodeDataToCSV("expData");
+
+
 	}
 	
 	public void Test()
@@ -89,7 +233,7 @@ public class Project3_Gridworld {
 	{
 		Project3_Gridworld example = new Project3_Gridworld();
 		String outputPath = "output/"; 
-		example.Test();
+		example.PolicyIterationExample(outputPath);
 	}
 	
 }
